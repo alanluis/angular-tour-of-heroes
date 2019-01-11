754c9ebf9b3af6c7cab5721dc04e8a8895cc1b9a
Alan Oliveira <alantechno@gmail.com>
2019-01-11 20:02:17 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/<name-of-app>'));
+app.use(express.static('/dist/angular-tour-of-heroes'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/<name-of-app>/index.html'));
+res.sendFile(path.join('/dist/angular-tour-of-heroes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
